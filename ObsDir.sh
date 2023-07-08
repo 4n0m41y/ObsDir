a0af37b8668ac4bad0fd69385a92fe206ebfe7a2
4n0m41y <4n0m41y@users.noreply.github.com>
2023-07-08 05:44:33 -0500

    Create ObsDir.sh

---
+++ ObsDir.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+check_url() {
+    response=$(curl -s --head $1 | head -n 1)
+    if [[ $response =~ "200 OK" ]]; then
+        echo "Subdirectory found: $1"
+    fi
+}
+
+read -p "Enter a domain: " domain
+domain="${domain%/}"
+
+subdirs=(
+    "/admin"
+    "/login"
+    "/wp-admin"
+    "/wp-login"
+    "/administrator"
+    "/phpmyadmin"
+    "/add"
+    "/more"
+    "prefixes"
+)
+
+for subdir in "${subdirs[@]}"
+do
+    check_url "$domain$subdir"
+done
